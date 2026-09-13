387c665d259ec5c3c2198460c710423f45dd5f5f
Vi <Pornoenot@gmail.com>
2026-09-13 19:16:37 +0700

    Build 113: update updater banner only

--- update.bat
+++ update.bat
@@ -29,7 +29,7 @@ set "STAGE=%BACKUP_ROOT%\_stage_%STAMP%"
 
 echo.
 echo ========================================
-echo        FPChat safe update, build 112
+echo        FPChat safe update, build 113
 echo ========================================
 echo.
 
