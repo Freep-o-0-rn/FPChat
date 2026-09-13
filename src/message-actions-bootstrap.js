3f4faf0ee75839cb21195d1785703708cabf2fc3
Vi <Pornoenot@gmail.com>
2026-09-13 20:15:44 +0700

    Build 116: install typing server layer

--- src/message-actions-bootstrap.js
+++ src/message-actions-bootstrap.js
@@ -1,4 +1,4 @@
-/* Build 112: inject isolated message-actions and message-pins server layers without rewriting server.js. */
+/* Build 116: inject isolated message-actions, message-pins and typing server layers without rewriting server.js. */
 const fs = require('fs');
 const path = require('path');
 const Module = require('module');
@@ -6,7 +6,7 @@ const Module = require('module');
 const target = path.resolve(__dirname, '..', 'server.js');
 const originalJsLoader = Module._extensions['.js'];
 
-Module._extensions['.js'] = function fpchatBuild112Loader(module, filename) {
+Module._extensions['.js'] = function fpchatBuild116Loader(module, filename) {
   if (path.resolve(filename) !== target) return originalJsLoader(module, filename);
 
   // Restore Node's normal loader before compiling server.js so all of its own
@@ -16,10 +16,10 @@ Module._extensions['.js'] = function fpchatBuild112Loader(module, filename) {
   let source = fs.readFileSync(filename, 'utf8');
   const marker = '\ncleanupExpiredSoloRooms();\nsetInterval(cleanupExpiredSoloRooms, 10 * 60 * 1000);\nserver.listen(APP_PORT, APP_HOST, () => console.log(`FPChat listening on http://${APP_HOST}:${APP_PORT}`));';
   if (!source.includes(marker)) {
-    throw new Error('FPChat Build 112 bootstrap: server.js startup marker was not found');
+    throw new Error('FPChat Build 116 bootstrap: server.js startup marker was not found');
   }
 
-  const install = `\nrequire('./src/message-actions-server').installMessageActionsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  broadcastUnreadState,\n  toIsoUtc,\n  safeUnlink,\n  isRoomOpen,\n  roomStatePayload\n});\n\nrequire('./src/message-pins-server').installMessagePinsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  toIsoUtc,\n  isRoomOpen,\n  roomStatePayload\n});\n`;
+  const install = `\nrequire('./src/message-actions-server').installMessageActionsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  broadcastUnreadState,\n  toIsoUtc,\n  safeUnlink,\n  isRoomOpen,\n  roomStatePayload\n});\n\nrequire('./src/message-pins-server').installMessagePinsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  toIsoUtc,\n  isRoomOpen,\n  roomStatePayload\n});\n\nrequire('./src/typing-server').installTypingServer({\n  wss,\n  q,\n  sendToRoomParticipants,\n  isRoomOpen\n});\n`;
 
   source = source.replace(marker, `${install}${marker}`);
   module._compile(source, filename);
