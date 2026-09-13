390cd1452e50c55c25711a80ba9ac3e42a27a20a
Vi <Pornoenot@gmail.com>
2026-09-13 19:01:15 +0700

    Build 112: load message pins server layer

--- src/message-actions-bootstrap.js
+++ src/message-actions-bootstrap.js
@@ -1,4 +1,4 @@
-/* Build 108: inject the isolated message-actions server layer without rewriting server.js. */
+/* Build 112: inject isolated message-actions and message-pins server layers without rewriting server.js. */
 const fs = require('fs');
 const path = require('path');
 const Module = require('module');
@@ -6,20 +6,20 @@ const Module = require('module');
 const target = path.resolve(__dirname, '..', 'server.js');
 const originalJsLoader = Module._extensions['.js'];
 
-Module._extensions['.js'] = function fpchatBuild108Loader(module, filename) {
+Module._extensions['.js'] = function fpchatBuild112Loader(module, filename) {
   if (path.resolve(filename) !== target) return originalJsLoader(module, filename);
 
   // Restore Node's normal loader before compiling server.js so all of its own
-  // dependencies load exactly as they did before Build 108.
+  // dependencies load exactly as before the isolated feature layers.
   Module._extensions['.js'] = originalJsLoader;
 
   let source = fs.readFileSync(filename, 'utf8');
   const marker = '\ncleanupExpiredSoloRooms();\nsetInterval(cleanupExpiredSoloRooms, 10 * 60 * 1000);\nserver.listen(APP_PORT, APP_HOST, () => console.log(`FPChat listening on http://${APP_HOST}:${APP_PORT}`));';
   if (!source.includes(marker)) {
-    throw new Error('FPChat Build 108 bootstrap: server.js startup marker was not found');
+    throw new Error('FPChat Build 112 bootstrap: server.js startup marker was not found');
   }
 
-  const install = `\nrequire('./src/message-actions-server').installMessageActionsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  broadcastUnreadState,\n  toIsoUtc,\n  safeUnlink,\n  isRoomOpen,\n  roomStatePayload\n});\n`;
+  const install = `\nrequire('./src/message-actions-server').installMessageActionsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  broadcastUnreadState,\n  toIsoUtc,\n  safeUnlink,\n  isRoomOpen,\n  roomStatePayload\n});\n\nrequire('./src/message-pins-server').installMessagePinsServer({\n  app,\n  db,\n  q,\n  socketsByDevice,\n  sendWsJson,\n  sendToRoomParticipants,\n  toIsoUtc,\n  isRoomOpen,\n  roomStatePayload\n});\n`;
 
   source = source.replace(marker, `${install}${marker}`);
   module._compile(source, filename);
