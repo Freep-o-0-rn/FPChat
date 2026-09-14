9c825d85e98f71ca49251068f91cfbdd4f50284e
Vi <Pornoenot@gmail.com>
2026-09-15 01:40:35 +0700

    Build 151: update updater banner

--- update.bat
+++ update.bat
@@ -29,7 +29,7 @@ set "STAGE=%BACKUP_ROOT%\_stage_%STAMP%"
 
 echo.
 echo ========================================
-echo        FPChat safe update, build 150
+echo        FPChat safe update, build 151
 echo ========================================
 echo.
 
