cc9e72219c67084568073047e3b3e3f1e77a7576
Vi <Pornoenot@gmail.com>
2026-09-15 12:14:18 +0700

    Build 156: bump updater banner

--- update.bat
+++ update.bat
@@ -29,7 +29,7 @@ set "STAGE=%BACKUP_ROOT%\_stage_%STAMP%"
 
 echo.
 echo ========================================
-echo        FPChat safe update, build 155
+echo        FPChat safe update, build 156
 echo ========================================
 echo.
 
