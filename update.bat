def457d5c993e79df71583c72da8bf62b9f9211f
Vi <Pornoenot@gmail.com>
2026-09-18 12:04:30 +0700

    Build 172: update updater label

--- update.bat
+++ update.bat
@@ -29,7 +29,7 @@ set "STAGE=%BACKUP_ROOT%\_stage_%STAMP%"
 
 echo.
 echo ========================================
-echo        FPChat safe update, build 171
+echo        FPChat safe update, build 172
 echo ========================================
 echo.
 
