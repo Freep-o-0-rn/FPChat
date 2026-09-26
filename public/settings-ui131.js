180465bac989fb161e684b1632303da04dca5968
Vi <Pornoenot@gmail.com>
2026-09-26 20:02:39 +0700

    build 188.1: update settings build label

--- public/settings-ui131.js
+++ public/settings-ui131.js
@@ -3,7 +3,7 @@
   if (window.__fpSettings131LoaderStarted) return;
   window.__fpSettings131LoaderStarted = true;
 
-  const BUILD = '187.1';
+  const BUILD = '188.1';
   let attempts = 0;
 
   const boot = () => {
